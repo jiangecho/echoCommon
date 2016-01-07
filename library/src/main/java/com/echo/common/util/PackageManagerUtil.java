1b9a18a96ff74bcbd23df2997981936f5959c580
jiangecho <jiangyingji1@163.com>
2016-01-07 10:48:56 +0800

    add PackageManagerUtil

---
+++ library/src/main/java/com/echo/common/util/PackageManagerUtil.java
@@ -0,0 +1,111 @@
+package com.echo.common.util;
+
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.text.TextUtils;
+
+import java.util.List;
+
+/**
+ * Created by jiangecho on 15/5/7.
+ */
+public class PackageManagerUtil {
+    public static boolean startApp(Context context, String packageName, String className) {
+
+        boolean result = false;
+        if (context == null || TextUtils.isEmpty(packageName) || TextUtils.isEmpty(className)) {
+            return false;
+        }
+
+        try {
+            context.getPackageManager().getApplicationInfo(packageName, PackageManager.GET_ACTIVITIES);
+
+            Intent intent = new Intent();
+            ComponentName componentName = new ComponentName(packageName, className);
+            intent.setAction(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setComponent(componentName);
+            context.startActivity(intent);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+        }
+        return result;
+    }
+
+    public static String getVersion(Context context) {
+        try {
+            PackageManager manager = context.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            String version = info.versionName;
+            return version;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "unknown";
+        }
+    }
+
+    public static int getVersionCode(Context context){
+        PackageManager packageManager = context.getPackageManager();
+        int versionCode = -1;
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            versionCode = packageInfo.versionCode;
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return versionCode;
+    }
+
+    public static boolean isActivityForground(Context context, String activityClassName) {
+        if (context == null || TextUtils.isEmpty(activityClassName)) {
+            return false;
+        }
+
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> taskInfos = am.getRunningTasks(1);
+        ComponentName componentName;
+        if (taskInfos != null && !taskInfos.isEmpty()) {
+            componentName = taskInfos.get(0).topActivity;
+            if (activityClassName.equals(componentName.getClassName())) {
+                return true;
+            }
+
+        }
+        return false;
+
+    }
+
+
+    public static String getAppMetaData(Context context, String key){
+        if (context == null || TextUtils.isEmpty(key)){
+            return null;
+        }
+
+        String value = null;
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+            if (applicationInfo != null && applicationInfo.metaData != null){
+                Object object = applicationInfo.metaData.get(key);
+                if (object != null){
+                    value = object.toString();
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return value;
+    }
+
+}
