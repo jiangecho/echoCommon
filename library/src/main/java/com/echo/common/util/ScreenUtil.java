dc2d7df8849627e55dd4507ba996598f4453046c
jiangecho <jiangyingji1@163.com>
2016-02-25 17:18:51 +0800

    add netUtil

--- library/src/main/java/com/echo/common/util/ScreenUtil.java
+++ library/src/main/java/com/echo/common/util/ScreenUtil.java
@@ -3,6 +3,7 @@ package com.echo.common.util;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.util.DisplayMetrics;
 import android.view.Surface;
 import android.view.WindowManager;
 
@@ -30,5 +31,23 @@ public class ScreenUtil {
 
     public static void enableScreenRotation(Activity activity) {
         activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+    }
+
+    public static int getScreenWidth(Activity activity) {
+        DisplayMetrics dm = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int mScreenWidth = dm.widthPixels;// 获取屏幕分辨率宽度
+        int mScreenHeight = dm.heightPixels;
+
+        return mScreenWidth;
+    }
+
+    public static int getScreenHeight(Activity activity) {
+        DisplayMetrics dm = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int mScreenWidth = dm.widthPixels;// 获取屏幕分辨率宽度
+        int mScreenHeight = dm.heightPixels;
+
+        return mScreenHeight;
     }
 }
